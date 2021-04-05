44def0a9388d44d3724a82bb08abbe347667e2ae
pacbel <suporte@sistemavirtual.com.br>
2021-04-05 19:27:57 -0300

    202104051927

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System; 
+
+class minhaClasse {
+
+    static void Main(string[] args) { 
+
+            int qtdTeste = int.Parse(Console.ReadLine());
+            string v1, v2;
+            for (int i = 1; i <= qtdTeste; i++) 
+            {
+                string[] valores = Console.ReadLine().Split();
+                v1 = valores[0];
+                v2 = valores[1];
+                if((      ) || (    ) //complete a solução
+                    || (     ) || (     )
+                    || (    ) || (    )
+                    || (     ) || (    )
+                    || (     ) || (   ))
+                    Console.WriteLine("Caso #{0}: Bazinga!", i);                
+                else if (    )   //complete a solucao
+                    Console.WriteLine("Caso #{0}: De novo!", i);
+                else
+                    Console.WriteLine("Caso #{0}: Raj trapaceou!", i);
+            }
+
+    }
+
+}
