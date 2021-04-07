1cb11ed74888a8ef7de04dbbc8ecc40a4bc1e4e3
pacheco <pacheco@MacBook.local>
2021-04-06 21:56:37 -0300

    202104062156

--- Program.cs
+++ Program.cs
@@ -1,28 +1,18 @@
-﻿using System; 
+﻿using System;
+using System.Collections.Generic;
 
 class minhaClasse {
+    static void Main(string[] args) {
+      int qtdTeste = int.Parse(Console.ReadLine());
+      var result = new Dictionary<int, string> {{0, "De novo!"},{1, "Bazinga!"}, {2, "Raj trapaceou!"}};
+      var tipos = new Dictionary<string, int> {{"tesoura", 0},{"papel", 1}, {"pedra", 2}, {"lagarto", 3}, {"Spock",4}};
+      int[,] escolhas =  new int[5,5] {{0,1,2,1,2}, {2,0,1,2,1}, {1,2,0,1,2}, {2,1,2,0,1}, {1,2,1,2,0}};
 
-    static void Main(string[] args) { 
+      for (int i = 1; i <= qtdTeste; i++)
+      {
 
-            int qtdTeste = int.Parse(Console.ReadLine());
-            string v1, v2;
-            for (int i = 1; i <= qtdTeste; i++) 
-            {
-                string[] valores = Console.ReadLine().Split();
-                v1 = valores[0];
-                v2 = valores[1];
-                if((      ) || (    ) //complete a solução
-                    || (     ) || (     )
-                    || (    ) || (    )
-                    || (     ) || (    )
-                    || (     ) || (   ))
-                    Console.WriteLine("Caso #{0}: Bazinga!", i);                
-                else if (    )   //complete a solucao
-                    Console.WriteLine("Caso #{0}: De novo!", i);
-                else
-                    Console.WriteLine("Caso #{0}: Raj trapaceou!", i);
-            }
-
+        string[] valores = Console.ReadLine().Split();
+        Console.WriteLine("Caso #{0}: {1}", i, result[escolhas[tipos[valores[0]],tipos[valores[1]]]]);
+      }
     }
-
 }
